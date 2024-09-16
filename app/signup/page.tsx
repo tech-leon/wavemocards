d83b64fc41ae6a5e032e1356b691af09dd40abe8
Leon <107266694+tech-leon@users.noreply.github.com>
2024-09-16 22:11:06 +0800

    update

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,7 @@
 'use client'
-// import { useTranslation } from "react-i18next";
 import SignupForm from "@/components/signup-form";
 
 export default function AuthPage() {
-  // const { t } = useTranslation();
   return (
     <div>
       <SignupForm />
