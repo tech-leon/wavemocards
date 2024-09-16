9c39680ebf3c59555f5bc929bcc94bb628740229
Leon <107266694+tech-leon@users.noreply.github.com>
2024-09-17 00:03:35 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-// import { useTranslation } from "react-i18next";
 import Hero from "@/components/hero";
 
 export default function Home() {
-  // const { t } = useTranslati/on();
   return (
     <main className="flex h-full flex-col items-center justify-center">
-        <Hero />
+      <Hero />
     </main>
   );
 }
--- components/emoCards/CategorySection.tsx
+++ components/emoCards/CategorySection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
-import EmoCard, { CardProps } from "./card";
+import EmoCard, { CardProps } from "./Card";
 import {
   ArrowDownRightIcon,
   ArrowLeftIcon,
