6949f35fecbf7c31142c93738cd605b02383cb06
Leon <107266694+tech-leon@users.noreply.github.com>
2024-09-28 11:41:25 +0800

    Add exit

--- backend.sh
+++ backend.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# 停止並移除容器
 docker compose down
 
-# 移除鏡像
 docker rmi wavemocards-backend:1.6
 
 rm -rf ./backend_code
 
-# 重新構建鏡像
 cp ../newenv/.env.compose .
 docker compose up -d
 
 sudo chown -R ubuntu:ubuntu ./backend_code
 cp ../newenv/wavemocards.json ../newenv/.env.app ./backend_code/
+
+exit 0
