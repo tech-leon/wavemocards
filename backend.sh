a77cdf194403eea8098e7705e5089bb43b55d8ac
Leon <107266694+tech-leon@users.noreply.github.com>
2024-09-28 12:00:24 +0800

    Change sleep to after copy

--- backend.sh
+++ backend.sh
@@ -9,9 +9,11 @@ rm -rf ./backend_code
 cp ../newenv/.env.compose .
 docker compose up -d
 
-sleep 1
-
 sudo chown -R ubuntu:ubuntu ./backend_code
 cp ../newenv/wavemocards.json ../newenv/.env.app ./backend_code/
+
+sleep 1
+
+sudo chown ubuntu:ubuntu ./backend_code/sql_app.db
 
 exit 0
