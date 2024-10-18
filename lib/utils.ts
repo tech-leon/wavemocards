3eda55e7d5b80cb0146b48943d967ab162d7f568
Leon <107266694+tech-leon@users.noreply.github.com>
2024-10-18 19:25:04 +0800

    Add a catchError function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function catchError<T>(promise: Promise<T>): Promise<[T, undefined] | [Error]> {
+  try {
+    const data = await promise
+    return [data, undefined]
+  } catch (error) {
+    return [error as Error]
+  }
+}
