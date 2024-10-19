4bc75642b27f46f91b90f28f61a00c6946910d38
Leon <107266694+tech-leon@users.noreply.github.com>
2024-10-19 22:01:24 +0800

    Remove {" "}

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,48 @@
-import localFont from "next/font/local";
-import "./globals.css";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import { ThemeProvider } from "@/components/theme-provider";
-import { AuthProvider } from "@/lib/auth/authContext";
-import "@/i18n/config";
-import { NextUIProvider } from "@nextui-org/react";
+import localFont from "next/font/local"
+import "./globals.css"
+import Header from "@/components/header"
+import Footer from "@/components/footer"
+import { ThemeProvider } from "@/components/theme-provider"
+import { AuthProvider } from "@/lib/auth/authContext"
+import "@/i18n/config"
+import { NextUIProvider } from "@nextui-org/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-});
+})
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-});
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AuthProvider>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
-            disableTransitionOnChange
-          >
+            disableTransitionOnChange>
             <div className="flex flex-col flex-grow">
               <NextUIProvider>
                 <Header />
-                {children}{" "}
+                {children}
               </NextUIProvider>
             </div>
             <Footer />
-          </ThemeProvider>{" "}
+          </ThemeProvider>
         </AuthProvider>
       </body>
     </html>
-  );
+  )
 }
